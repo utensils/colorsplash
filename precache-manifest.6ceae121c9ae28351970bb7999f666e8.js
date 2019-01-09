54591a3e76fad0546b494da299a9a46aa0288620
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-09 20:03:50 +0000

    Deploy utensils/colorsplash to github.com/utensils/colorsplash.git:gh-pages

---
+++ precache-manifest.6ceae121c9ae28351970bb7999f666e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e5dbba437303a34a5a1",
+    "url": "/colorsplash/static/css/main.df339c4d.chunk.css"
+  },
+  {
+    "revision": "1e5dbba437303a34a5a1",
+    "url": "/colorsplash/static/js/main.1e5dbba4.chunk.js"
+  },
+  {
+    "revision": "cf2ecd9a5af3a402b09b",
+    "url": "/colorsplash/static/css/1.5de9ad35.chunk.css"
+  },
+  {
+    "revision": "cf2ecd9a5af3a402b09b",
+    "url": "/colorsplash/static/js/1.cf2ecd9a.chunk.js"
+  },
+  {
+    "revision": "75099a6b2b5551ded3ea",
+    "url": "/colorsplash/static/js/runtime~main.75099a6b.js"
+  },
+  {
+    "revision": "b2562dea4699fb3d7882d65e03065500",
+    "url": "/colorsplash/index.html"
+  }
+];
