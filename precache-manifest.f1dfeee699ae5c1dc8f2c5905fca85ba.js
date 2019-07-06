068afaed308d7729d4da59109f4596459d4509da
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-06 21:12:53 +0000

    Deploy utensils/colorsplash to github.com/utensils/colorsplash.git:gh-pages

---
+++ precache-manifest.f1dfeee699ae5c1dc8f2c5905fca85ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8918bcb449cea32ce6091df04507e9a",
+    "url": "/colorsplash/index.html"
+  },
+  {
+    "revision": "e46cde14894a2e05ce6c",
+    "url": "/colorsplash/static/css/2.1ef35742.chunk.css"
+  },
+  {
+    "revision": "e4d1a9def2e3528faedc",
+    "url": "/colorsplash/static/css/main.01dbed32.chunk.css"
+  },
+  {
+    "revision": "e46cde14894a2e05ce6c",
+    "url": "/colorsplash/static/js/2.b0efd806.chunk.js"
+  },
+  {
+    "revision": "e4d1a9def2e3528faedc",
+    "url": "/colorsplash/static/js/main.298cb1e1.chunk.js"
+  },
+  {
+    "revision": "731fd09e64aad9640487",
+    "url": "/colorsplash/static/js/runtime~main.2a455c5a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/colorsplash/precache-manifest.3254b3976ff848862d5aa1d93f88ca28.js"
+  "/colorsplash/precache-manifest.f1dfeee699ae5c1dc8f2c5905fca85ba.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/colorsplash/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/colorsplash/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
